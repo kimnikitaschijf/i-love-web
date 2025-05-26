4b658b7ee4ac0ad7ba8705a94da2605834175b16
kimnikitaschijf <kimnikitaschijfhva@icloud.com>
2025-05-26 11:46:52 +0200

    #23 Nieuwe section toegevoegd aan home & footer

---
+++ public/assets/main.js
@@ -0,0 +1,15 @@
+// const observer = new IntersectionObserver(entries => {
+//     entries.forEach(entry => {
+//         const intersecting = entry.isIntersecting
+
+//         if (!intersecting) {
+//             document.documentElement.classList.add("uitbeeld");
+//         }
+//     })
+// })
+
+// let deDivs = document.querySelectorAll("div");
+
+// let deHeader = document.querySelector(".main-header")
+
+// observer.observe(deHeader);
